ac47f8fd6cb9e2ad50c3f7e75cf6f3b980159dd3
LiveXu pro <macbookpro@livexudemacbook-pro.local>
2016-05-24 10:39:33 +0800

    Initial Commit

---
+++ LXAutoCircleText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXAutoCircleText
+//
+//  Created by MacBook pro on 16/5/24.
+//  Copyright © 2016年 FZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXAutoCircleText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXAutoCircleText
+//
+//  Created by MacBook pro on 16/5/24.
+//  Copyright © 2016年 FZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
